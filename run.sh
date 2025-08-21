28d538dca1bb9d9c954e6956d357c27730d43641
Jeffrey in Tiamat <thw20@ic.ac.uk>
2025-08-21 06:49:15 +0100

    .

--- run.sh
+++ run.sh
@@ -19,17 +19,17 @@ python3 tpu_profiling_ttft.py \
     --num-iters-warmup 1
 
 
-export XLA_HLO_DEBUG=1
-export MODEL=RedHatAI/Meta-Llama-3.1-8B-Instruct-quantized.w8a8
-export VLLM_TPU_PROFILE_DURATION_MS=3000
-export VLLM_TPU_PROFILE_DELAY_MS=0
+# export XLA_HLO_DEBUG=1
+# export MODEL=RedHatAI/Meta-Llama-3.1-8B-Instruct-quantized.w8a8
+# export VLLM_TPU_PROFILE_DURATION_MS=3000
+# export VLLM_TPU_PROFILE_DELAY_MS=0
 
-python3 tpu_profiling_ttft.py \
-    --model $MODEL \
-    --input-len 5600 \
-    --output-len 83000 \
-    --batch-size 4 \
-    --enforce-eager \
-    --profile-result-dir profiles \
-    --tensor-parallel-size 8 \
-    --num-iters-warmup 1
+# python3 tpu_profiling_ttft.py \
+#     --model $MODEL \
+#     --input-len 5600 \
+#     --output-len 83000 \
+#     --batch-size 4 \
+#     --enforce-eager \
+#     --profile-result-dir profiles \
+#     --tensor-parallel-size 8 \
+#     --num-iters-warmup 1
